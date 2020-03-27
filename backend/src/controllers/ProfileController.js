82503f6a9d2d9c42a3d9b0845cec9da2e1644f98
Lucas Santana <lucas.santana@xpand-it.com>
2020-03-27 00:24:43 +0000

    Frontend Pages

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,23 +1,24 @@
 const conn = require('../database/connection');
 
 module.exports = {
-  async Index(request,reponse) {
-    const ong_id = request.headers.authorization;
-    const incidents = await conn('incidents')
-    .join('ong','ong.id', '=','incidents.ong_id' )
-    .where('ong_id',ong_id).select(['incidents.*',
-    'ong.name',
-    'ong.email',
-    'ong.whatsapp',
-    'ong.city',
-    'ong.uf']).limit(5).offset((page - 1)*5);
-      
-    const [count] = await conn('incidents').select('*').count();
-      
-    reponse.header('X-Total-Count',count['count(*)']);
-    reponse.header('X-Total-Pages-Count',count['count(*)'] / 5);   
-          reponse.json(incidents);
+    async Index(request, reponse) {
+        const ong_id = request.headers.authorization;
 
-    return reponse.json(incidents);
-   }
+        const incidents = await conn('incidents')
+            .join('ong', 'ong.id', '=', 'incidents.ong_id')
+            .where('ong_id', ong_id).select(['incidents.*',
+                'ong.name',
+                'ong.email',
+                'ong.whatsapp',
+                'ong.city',
+                'ong.uf']);
+
+        const [count] = await conn('incidents').select('*').count();
+
+        reponse.header('X-Total-Count', count['count(*)']);
+        reponse.header('X-Total-Pages-Count', count['count(*)'] / 5);
+        reponse.json(incidents);
+
+        return reponse.json(incidents);
+    }
 };
--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,14 +1,14 @@
 const conn = require('../database/connection');
 
 module.exports = {
-    async Create(request,reponse){
+    async Create(request,response){
         const {id} = request.body;
 
         const ong = await conn('ong').where('id',id).select('name').first();
 
         if(!ong)
-          return reponse.status(400).json({error: "No ONG founded with this ID"});
+          return response.status(400).json({error: "No ONG founded with this ID"});
 
-        return reponse.json(ong);  
+        return response.json(ong);
     }
-}
+};
